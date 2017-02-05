ebcfa0ef1175eb005552736d11f016312d0a7c0d
samy440 <samy.coulombe@mail.mcgill.ca>
2017-02-05 16:01:33 -0500

    Update sort_alphabetically.py
    Added cmdline arg option.

--- sort_alphabetically.py
+++ sort_alphabetically.py
@@ -1,17 +1,28 @@
+import sys
+
 def main():
-	outfile = open("MGH New Volunteers.txt", "w")
-	infile = open("vols.txt", "r")
-	listOfVolunteers = infile.readlines()
-	# sortedListOfNames = sortAlphabetically(listOfVolunteers)
-	sortedListOfNames = sorted(listOfVolunteers)
+	outfileName = "defaultOutputFile.txt"
+	infileName = "defaultInputFile.txt"
+	# cmdline parsing override if-block
+	if (len(sys.argv) >= 3):
+		#print("Provide the following input >> <name of file to sort alphabetically>.<extension> [space] <name of output file>.<extension>")
+		print("Number of arguments: ", len(sys.argv), "arguments.")
+		print("Input file: ", str(sys.argv[1]), ", output file: ", str(sys.argv[2]), ".")
+		outfileName = str(sys.argv[2])
+		infileName = str(sys.argv[1])
+	outfile = open(outfileName, "w")
+	infile = open(infileName, "r")
+	listOfNames = infile.readlines()
+	# sortedListOfNames = sortAlphabetically(listOfNames)
+	sortedListOfNames = sorted(listOfNames)
 	makeAFileWith(sortedListOfNames, outfile)
 	outfile.close()
 	infile.close()
-
+	print("Done.")
 def makeAFileWith(listOfNames, outfile): 
 	x = 1
 	for name in listOfNames:
-		stringToAdd = str(x) + ". " + str(name) + '\n'
+		stringToAdd = str(x) + ". " + str(name)
 		print(stringToAdd)
 		outfile.write(stringToAdd)
 		x += 1
